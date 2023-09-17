a9a4856fd6032028604cb7371b6b7c6d4e14ac5b
Luiz carlos mendes <82839686+luizcarlosmendes1308@users.noreply.github.com>
2023-09-17 19:08:26 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace ED1I4.Atividade4
+{
+    static class Program
+    {
+    /*
+     *PARTICIPANTES
+     * LUIZ CARLOS MENDES CB3012123
+    */
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
